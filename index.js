02f16199d43c696f37e9f21b9a527d19a40e8156
Karan Lakshminarayanan <karan@Karans-MacBook-Air.local>
2025-11-26 04:05:50 -0500

    fix session cookie config

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (process.env.SERVER_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.SERVER_URL,
+    // Removed domain - not needed for cross-site cookies
   };
 }
 
