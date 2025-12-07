5532efc9cfa0f60c6a306f1994fb1d65f32a36d8
Karan Lakshminarayanan <karan@Karans-MacBook-Air.local>
2025-12-07 00:15:43 -0500

    Fix MongoDB connection handling

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 
 const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
-mongoose.connect(CONNECTION_STRING);
+
+mongoose.connect(CONNECTION_STRING, {
+  serverSelectionTimeoutMS: 30000,
+})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err.message));
 
 const app = express();
 
